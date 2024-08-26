45c0ad538c9748d4385afa2c035f9f4b6c79017f
Blooym <19539165+Blooym@users.noreply.github.com>
2024-08-26 09:27:27 +0100

    chore: add wording to installer complete

--- setup/install-flatpak.sh
+++ setup/install-flatpak.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "-- XLM Flatpak Auto-Installer --"
+echo ""
 
 echo "[Step: 1] Downloading XLM"
 curl -L https://github.com/Blooym/xlm/releases/latest/download/xlm > /tmp/xlm
@@ -11,3 +12,6 @@ chmod +x /tmp/xlm
 
 echo "[Step: 3] Cleanup XLM binary"
 rm /tmp/xlm
+
+echo ""
+echo "-- Auto Installer Complete: Restart Steam and follow the README to continue! --"
--- setup/install-steamdeck.sh
+++ setup/install-steamdeck.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "-- XLM Steam Deck Auto-Installer --"
+echo ""
 
 echo "[Step: 1] Downloading XLM"
 curl -L https://github.com/Blooym/xlm/releases/latest/download/xlm > /tmp/xlm
@@ -11,3 +12,6 @@ chmod +x /tmp/xlm
 
 echo "[Step: 3] Cleanup XLM binary"
 rm /tmp/xlm
+
+echo ""
+echo "-- Auto Installer Complete: Go back to gaming mode and follow the README to continue! --"
