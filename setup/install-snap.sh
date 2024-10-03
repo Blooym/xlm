6bc97b8a9d17f83c578d0baf923cddf1e907ef85
Blooym <19539165+Blooym@users.noreply.github.com>
2024-10-03 23:25:05 +0100

    fix: snap path

--- setup/install-snap.sh
+++ setup/install-snap.sh
@@ -8,7 +8,7 @@ curl -L https://github.com/Blooym/xlm/releases/latest/download/xlm > /tmp/xlm
 
 echo "[Step: 2] Configuring XLM as a Steam Tool"
 chmod +x /tmp/xlm
-/tmp/xlm install-steam-tool --steam-compat-path ~/snap/steam/.steam/root/compatibilitytools.d/
+/tmp/xlm install-steam-tool --steam-compat-path ~/snap/steam/common/.steam/root/compatibilitytools.d/
 
 echo "[Step: 3] Cleanup XLM binary"
 rm /tmp/xlm
