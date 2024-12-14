6e4b2f8ebdf59daa5d23010b056d93c9a834be8b
Blooym <19539165+Blooym@users.noreply.github.com>
2024-12-14 17:19:40 +0000

    feat: update scripts to set new flag

--- setup/install-native.sh
+++ setup/install-native.sh
@@ -14,7 +14,7 @@ curl --fail -L https://github.com/Blooym/xlm/releases/latest/download/xlm-x86_64
 
 echo "[Step: 2] Configuring XLM as a Steam Tool"
 chmod +x /tmp/xlm
-/tmp/xlm install-steam-tool --xlm-updater-disable --steam-compat-path ~/.steam/root/compatibilitytools.d/
+/tmp/xlm install-steam-tool --xlm-updater-disable --extra-launch-args="--run-as-steam-compat-tool=true" --steam-compat-path ~/.steam/root/compatibilitytools.d/
 
 echo "[Step: 3] Cleanup XLM binary"
 rm /tmp/xlm
--- setup/install-snap.sh
+++ setup/install-snap.sh
@@ -3,6 +3,12 @@ set -e
 
 echo "-- XLM Snap Auto-Installer --"
 echo
+echo "-----"
+echo "IMPORTANT:"
+echo "Snap is considered an unsupported and experimental install type. XLM might work fine, or it may not work at all."
+echo "If you run into issues, you should consider installing Steam a different way, like from a native package."
+echo "------"
+echo
 
 if [ "$(id -u)" -eq 0 ]; then
     echo 'This script cannot be ran as the root user or with sudo. Please run it as a regular user.'
@@ -14,7 +20,7 @@ curl --fail -L https://github.com/Blooym/xlm/releases/latest/download/xlm-x86_64
 
 echo "[Step: 2] Configuring XLM as a Steam Tool"
 chmod +x /tmp/xlm
-/tmp/xlm install-steam-tool --xlm-updater-disable --steam-compat-path ~/snap/steam/common/.steam/root/compatibilitytools.d/
+/tmp/xlm install-steam-tool --xlm-updater-disable --extra-launch-args="--run-as-steam-compat-tool=true" --steam-compat-path ~/snap/steam/common/.steam/root/compatibilitytools.d/
 
 echo "[Step: 3] Cleanup XLM binary"
 rm /tmp/xlm
